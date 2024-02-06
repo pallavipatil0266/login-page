3e1a04335062d9dced68ba3945feca086cc45e99
pallavi patil <pallavipatil0266@gmail.com>
2024-02-06 18:02:07 +0530

    first commit

--- mail.js
+++ mail.js
@@ -1,22 +1,35 @@
 import nodemailer from 'nodemailer';
 import express from 'express';
+import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser'
 
 const app = express();
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: '',
-      pass: ''
-    }
-  });
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const __filesname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__filesname, 'public')));
+
+
+app.post('/login', function (req, res){
+  var x = req.body.to
+  var y = req.body.sub
+  var z = req.body.text
+  var p = "Thank you, "+ x + " "+ y + " " + z + ". we will contact you soon";
+   console.log(x)
+   console.log(y)
+   console.log(z)
+   console.log(p)
+   res.render("login.ejs", {"text":p })
   
-  let mailOptions = {
-    from: '',
-    to: '',
-    subject: 'Sending Email using Node.js',
-    html: '<h1>Welcome</h1><p>That was easy!</p>'
+   let mailOptions = {
+    from: process.env.email,
+    to: x,
+    subject: y,
+    html: z
   };
-  
+
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log(error);
@@ -24,7 +37,26 @@ let transporter = nodemailer.createTransport({
       console.log('Email sent: ' + info.response);
     }
   });
+  });
+  
 
-  app.listen(6000, (req, res) => {
-    console.log('port is running on 6000')
-  })
+app.get('/login', function (req, res) {
+  console.log(__dirname)
+  res.render("login.ejs", {"text": 'Fill this Form to get Connected With Us'})
+
+});
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.email,
+      pass: process.env.pass
+    }
+  });
+  
+app.listen(5600, (req, res) => {
+    console.log('port is running on 5600'),
+    console.log(process.env.email),
+    console.log(process.env.pass)
+    
+  });
